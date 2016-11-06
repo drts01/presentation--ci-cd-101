8edc9211684eb0d6e1d1581e54793143561830fa
Carlos Meza <carlos@digitalr00ts.com>
2016-11-06 13:05:12 -0800

    fix stripping out external links from lorem-ipsum

--- DEMO/aliases.sh
+++ DEMO/aliases.sh
@@ -2,3 +2,4 @@
 
 alias jekyll='bundle exec jekyll'
 alias travis='bundle exec travis'
+alias lorem-ipsum='curl --silent --show-error https://jaspervdj.be/lorem-markdownum/markdown.txt | sed -n "1h;2,\$H;\${g;s/\[.*\](http.*)//g;p}"'
